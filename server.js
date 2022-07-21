4144cb1f7353f9d60db4d3a0e97f1f60c80d428e
s-jain-codes <s.jain.codes@gmail.com>
2022-07-21 15:19:11 +0530

    create groups

--- server.js
+++ server.js
@@ -76,6 +76,17 @@ app.get("/groups", (req, res) => {
   });
 });
 
+app.post("/groups/new", (req, res) => {
+  const groupData = req.body;
+  Groups.create(groupData, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
 app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
 
